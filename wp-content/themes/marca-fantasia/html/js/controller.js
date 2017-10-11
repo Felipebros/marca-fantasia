c6ccc9ee1c1cd3e612fa1f883e4ef325d040504b
Matheus B <matheusbelt@hotmail.com>
2017-10-11 00:24:46 -0300

    tentativa do menu
    adicionei sass, controller js e tentei fazer o menu usando jquery

---
+++ wp-content/themes/marca-fantasia/html/js/controller.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $(".subnav-item").on('click', function(){
+    	$('.secondmenu').removeClass('hide');
+    	$('.secondmenu').addClass('translate');
+    });
+
+});
