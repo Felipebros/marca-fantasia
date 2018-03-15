c8e6bfaadcd2e9e619b22555e8b7a9164926dda6
interfaceslivres <paulohsms@gmail.com>
2018-03-15 11:55:55 -0300

    transformação em wordpress, index,header,footer

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'mf');
+define('DB_NAME', 'marca-fantasia');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'marca-fantasia');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'marca-fantasia');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
